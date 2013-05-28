49d8371f82e2fb83db55ad112c336bb95e814cdf
Austin Riba <austin.riba@gmail.com>
2013-05-27 17:20:29 -0700

    Add accelerometer reading

--- HowGnar/build/source/r/debug/com/toxiccode/howgnar/R.java
+++ HowGnar/build/source/r/debug/com/toxiccode/howgnar/R.java
@@ -20,9 +20,10 @@ public final class R {
         public static final int stationary=0x7f020002;
     }
     public static final class id {
-        public static final int action_settings=0x7f090002;
-        public static final int gnar_image=0x7f090001;
-        public static final int gnar_score=0x7f090000;
+        public static final int action_settings=0x7f090003;
+        public static final int gnar_count=0x7f090000;
+        public static final int gnar_image=0x7f090002;
+        public static final int gnar_score=0x7f090001;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
--- HowGnar/src/main/java/com/toxiccode/howgnar/RecordRideActivity.java
+++ HowGnar/src/main/java/com/toxiccode/howgnar/RecordRideActivity.java
@@ -3,22 +3,29 @@ package com.toxiccode.howgnar;
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.NavUtils;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
 
 import java.util.Calendar;
 
 /**
  * Created by austin on 5/22/13.
  */
-public class RecordRideActivity extends Activity {
+public class RecordRideActivity extends Activity implements SensorEventListener {
     public static final String GNARNESS = "com.toxiccode.howgnar.GNARNESS";
     private Calendar startCal;
     private Calendar endCal;
     private int gnars;
+    private SensorManager sensorManager;
+    double ax,ay,az;
 
     @SuppressLint("NewApi")
     @Override
@@ -32,6 +39,8 @@ public class RecordRideActivity extends Activity {
         }
         startCal = Calendar.getInstance();
         gnars = 0;
+        sensorManager=(SensorManager) getSystemService(SENSOR_SERVICE);
+        sensorManager.registerListener(this, sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_NORMAL);
 
         setContentView(R.layout.activity_record_ride);
     }
@@ -60,6 +69,26 @@ public class RecordRideActivity extends Activity {
         intent.putExtra(GNARNESS, gnarness);
         startActivity(intent);
 
+    }
+    @Override
+    public void onAccuracyChanged(Sensor arg0, int arg1) {
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (event.sensor.getType()==Sensor.TYPE_ACCELEROMETER){
+            ax=event.values[0];
+            ay=event.values[1];
+            az=event.values[2];
+            if((ax > 7 && ay > 7) || az > 16 || az < -7){
+                System.out.println("===SENSOR=== x: "+ax+" y: "+ ay + "z: " + az);
+                this.gnars++;
+                TextView gnarCount = (TextView)findViewById(R.id.gnar_count);
+                gnarCount.setText(""+gnars);
+            }
+
+
+        }
     }
 
 }
